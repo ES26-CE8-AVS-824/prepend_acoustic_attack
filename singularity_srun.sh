bd1bb0853ee1e0c72e867a6cf197b96fb04a930d
NT00PF <nt00pf@student.aau.dk@ailab-fe01.srv.aau.dk>
2026-03-03 15:02:56 +0100

    added prepend_attack venv script(s)

---
+++ singularity_srun.sh
@@ -0,0 +1,7 @@
+srun singularity exec --nv --writable-tmpfs \
+     -B /ceph/project/es26-ce8-avs-824/whispers-in-the-storm/extern/prepend_acoustic_attack/prepend_attack_venv:/scratch/prepend_attack_venv \
+     -B $HOME/.singularity:/scratch/singularity \
+     /ceph/container/python/python_3.9.sif \
+     /bin/bash -c "export TMPDIR=/scratch/singularity/tmp && \
+                   source /scratch/prepend_attack_venv/bin/activate && \
+                   nvidia-smi"
